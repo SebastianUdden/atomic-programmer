2f8865e052624ee0eec1f6327250f471f81cc8a6
Sebastian Uddén <sebastianudden@sebastians-mbp.lan>
2020-05-27 17:37:37 +0200

    feat: new path examples, cubic bezier

--- stories/SVG/path.stories.js
+++ stories/SVG/path.stories.js
@@ -22,6 +22,11 @@ const pathTemplates = [
   "Diagonal Line (M 10 10 L 90 90)",
   "Triangle (M 10 10 V 90 H 90 Z)",
   "Square (M 10 10 V 90 H 90 V 10 Z)",
+  "Downward Cubic Bezier (M 10 10 C 20 120, 80 120, 90 10)",
+  "Upward Cubic Bezier (M 10 90 C 20 -20, 80 -20, 90 90)",
+  "Rightward Cubic Bezier (M 10 10 C 120 20, 120 80, 10 90)",
+  "Leftward Cubic Bezier (M 90 10 C -20 20, -20 80, 90 90)",
+  "S-curve Cubic Bezier (M 10 50 C 10 0, 50 0, 50 50 S 90 100, 90 50)",
 ];
 
 export const aPath = () => {
